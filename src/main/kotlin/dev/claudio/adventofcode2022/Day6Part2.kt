eb47f6ff36e8b25eb14819ce4292c01dd610d675
claudio <claudio.consolmagno@rungway.com>
2022-12-06 16:35:40 +0000

    Day6Part2 Concise & Efficient

--- src/main/kotlin/dev/claudio/adventofcode2022/Day6Part2.kt
+++ src/main/kotlin/dev/claudio/adventofcode2022/Day6Part2.kt
@@ -6,14 +6,12 @@ fun main() {
 
 private class Day6Part2 {
     fun main() {
-        val input: CharArray = Support.readFileAsListString("2022/day6-input.txt")[0].toCharArray()
+        val input: String = Support.readFileAsListString("2022/day6-input.txt")[0]
         val wantedDistinctChars = 14
-        for (i in 0..input.size) {
-            val distinctChars = (0 until wantedDistinctChars).map { input[it + i] }.toSet().size
-            if (distinctChars == wantedDistinctChars) {
-                println(i + wantedDistinctChars)
-                return
-            }
-        }
+        val result = input.asSequence()
+            .windowed(wantedDistinctChars, 1)
+            .withIndex()
+            .find { it.value.distinct().size == wantedDistinctChars }!!.index + wantedDistinctChars
+        println(result)
     }
 }
